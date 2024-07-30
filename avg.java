2546890adf4e0f669eea8b04388cf78d1c48e4ea
peerada <naruapon@gmail.com>
2024-07-30 10:46:00 +0700

    avg.java

---
+++ avg.java
@@ -0,0 +1,8 @@
+public class avg {
+    public static void main(String[] args) {
+        byte num1=7;
+        byte num2=8;
+        float avg=(num1+num2)/2.00f;
+        System.out.print(avg);
+    }
+}
